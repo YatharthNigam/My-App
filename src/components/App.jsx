cd2a796401dc77357156279b16d3658bd11e45de
Yatharth Nigam <yatharthnigam@gmail.com>
2021-08-13 09:38:00 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Covid 19 Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant></Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default App;
